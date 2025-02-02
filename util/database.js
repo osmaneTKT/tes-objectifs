fdd17741ea1fa0f75285262ec290dc6a1617a327
osmaneTKT <osmane2002@hotmail.fr>
2025-02-02 12:57:36 +0100

    password

--- util/database.js
+++ util/database.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const pool = mysql.createPool({
     host: 'mysql-data.cav82m4sgg3b.us-east-1.rds.amazonaws.com',
     user: 'admin',
     database: 'node-complete',
-    password: 'Meknes1975'
+    password: process.env.DB_PASSWORD
 });
 
 module.exports = pool.promise();
